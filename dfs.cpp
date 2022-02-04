f4dd3be5e3c01e4c760946c9b198df5b35f2f252
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-04 12:25:05 -0400

    Update: Return

--- dfs.cpp
+++ dfs.cpp
@@ -1,21 +1,21 @@
 #include "dfs.hpp"
 
 Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
-    if (usedStates.find(root.state) != usedStates.end()){
-        return root;
-    }
-
+    usedStates.insert(root.state);
     for (Node expandedNode: root.expandNode(columns, rows)){
         expandedNode.cost ++;
+        // std::cout<< "Search cost: "<< expandedNode.cost<< std::endl;
+        // std::cout<< "State: "<< expandedNode.state<< std::endl;
+        if (usedStates.find(expandedNode.state) == usedStates.end()){
+            dfs(expandedNode, columns, rows, usedStates, targetState, searchCost);
+        }
         if (expandedNode.state == targetState){
             return expandedNode;
         }
-        expandedNode = dfs(expandedNode, columns, rows, usedStates, targetState, searchCost++);
     }
     // Return a node with the searchCost, but initial state and emptyPath
-    // Return a node with the searchCost, but initial state and emptyPath
     std::vector<std::string> emptyPath;
-    Node notFound = Node(root.state, *searchCost, emptyPath);
+    Node notFound = Node(root.state, root.cost, emptyPath);
 
     return notFound;
 }
