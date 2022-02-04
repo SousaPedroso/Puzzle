0a03df2ca3dba90f5b2eaa01341dbe68941afa9c
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-04 17:31:51 -0400

    Update: Iterative dfs

--- dfs.cpp
+++ dfs.cpp
@@ -1,21 +1,30 @@
 #include "dfs.hpp"
 
 Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
-    usedStates.insert(root.state);
-    for (Node expandedNode: root.expandNode(columns, rows)){
-        expandedNode.cost ++;
-        // std::cout<< "Search cost: "<< expandedNode.cost<< std::endl;
-        // std::cout<< "State: "<< expandedNode.state<< std::endl;
-        if (usedStates.find(expandedNode.state) == usedStates.end()){
-            dfs(expandedNode, columns, rows, usedStates, targetState, searchCost);
-        }
-        if (expandedNode.state == targetState){
-            return expandedNode;
+    std::stack<Node> nodeStack;
+    nodeStack.push(root);
+
+    while (!nodeStack.empty()){
+        // Get last Node
+        Node nextNode = nodeStack.top();
+        // Push his state
+        usedStates.insert(nextNode.state);
+        nodeStack.pop();
+        for (Node expandedNode: nextNode.expandNode(columns, rows)){
+            (*searchCost)++;
+            if (expandedNode.state == targetState){
+                return expandedNode;
+            }
+            if (usedStates.find(expandedNode.state) == usedStates.end()){
+                nodeStack.push(expandedNode);
+                usedStates.insert(expandedNode.state);
+            }
         }
     }
+
     // Return a node with the searchCost, but initial state and emptyPath
     std::vector<std::string> emptyPath;
-    Node notFound = Node(root.state, root.cost, emptyPath);
+    Node notFound = Node(root.state, *searchCost, emptyPath);
 
     return notFound;
 }
--- dfs.hpp
+++ dfs.hpp
@@ -2,5 +2,6 @@
 #include "Node.hpp"
 #endif
 #include <set>
+#include <stack>
 
 Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost);
