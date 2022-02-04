5a2d5a694ebf0545ffdde578ccd726f465f449c0
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-04 06:42:12 -0400

    Initial dfs

---
+++ dfs.cpp
@@ -0,0 +1,22 @@
+#include "Node.hpp"
+#include <set>
+
+Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
+    if (usedStates.find(root.state) != usedStates.end()){
+        return root;
+    }
+
+    for (Node expandedNode: root.expandNode(columns, rows)){
+        expandedNode.cost ++;
+        if (expandedNode.state == targetState){
+            return expandedNode;
+        }
+        expandedNode = dfs(expandedNode, columns, rows, usedStates, targetState, searchCost++);
+    }
+    // Return a node with the searchCost, but initial state and emptyPath
+    // Return a node with the searchCost, but initial state and emptyPath
+    std::vector<std::string> emptyPath;
+    Node notFound = Node(root.state, *searchCost, emptyPath);
+
+    return notFound;
+}
