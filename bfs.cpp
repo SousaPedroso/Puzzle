28493186b6a0ab49da82c1c7805473067893759d
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-06 10:47:10 -0400

    Update: Removed unnecessary coments

--- bfs.cpp
+++ bfs.cpp
@@ -8,14 +8,12 @@ Node bfs(Node root, int columns, int rows, std::set<std::string> usedStates, std
         Node nextNode = nodeQueue.front();
         // Push his state
         usedStates.insert(nextNode.state);
-        // std::cout<< "Root: "<< nextNode.state<< std::endl;
         nodeQueue.pop();
         for (Node expandedNode: nextNode.expandNode(columns, rows)){
             if (expandedNode.state == targetState){
                 return expandedNode;
             }
             if (usedStates.find(expandedNode.state) == usedStates.end()){
-                // std::cout<< "State: "<< expandedNode.state<<std::endl;
                 (*searchCost) ++;
                 nodeQueue.push(expandedNode);
                 usedStates.insert(expandedNode.state);
