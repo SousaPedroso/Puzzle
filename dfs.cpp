289b47359f18750c61e2a0a47f7ef7870c7001d0
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-11 08:50:51 -0400

    dfs break through hash

--- dfs.cpp
+++ dfs.cpp
@@ -1,6 +1,6 @@
 #include "dfs.hpp"
 
-Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
+Node dfs(Node root, int columns, int rows, std::set<int> usedStates, long long targetHash, int* searchCost, long long p, long long x){
     std::stack<Node> nodeStack;
     nodeStack.push(root);
 
@@ -8,31 +8,33 @@ Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std
         // Get last Node
         Node nextNode = nodeStack.top();
         // Push his state
-        usedStates.insert(nextNode.state);
+        usedStates.insert(nextNode.hash);
         nodeStack.pop();
         for (Node expandedNode: nextNode.expandNode(columns, rows)){
-            if (expandedNode.state == targetState){
+            expandedNode.hash = expandedNode.computeHash(p, x, rows, columns);
+            if (expandedNode.hash == targetHash){
                 return expandedNode;
             }
-            if (usedStates.find(expandedNode.state) == usedStates.end()){
+            if (usedStates.find(expandedNode.hash) == usedStates.end()){
                 (*searchCost)++;
                 nodeStack.push(expandedNode);
-                usedStates.insert(expandedNode.state);
+                usedStates.insert(expandedNode.hash);
             }
             // All possible states were seen (https://stackoverflow.com/questions/32983617/improve-8-puzzle-using-bfs)
             if (usedStates.size() >= 181440){
                 // Return a node with the searchCost, but initial state and emptyPath
                 std::vector<std::string> emptyPath;
-                std::vector<int> indices(rows*columns, 0);
+                std::array<int, 9> indices;
                 Node notFound = Node(root.state, *searchCost, emptyPath, indices);
                 return notFound;
             }
         }
+        // std::cout << nodeStack.size()<< endLine;
     }
 
     // Return a node with the searchCost, but initial state and emptyPath
     std::vector<std::string> emptyPath;
-    std::vector<int> indices(rows*columns, 0);
+    std::array<int, 9> indices;
     Node notFound = Node(root.state, *searchCost, emptyPath, indices);
 
     return notFound;
