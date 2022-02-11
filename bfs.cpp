7cdf9ad0c2d861bfa561678b00bbc925de164c2c
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-11 08:52:03 -0400

    bfs break through hash

--- bfs.cpp
+++ bfs.cpp
@@ -1,28 +1,31 @@
 #include "bfs.hpp"
 
-Node bfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
+Node bfs(Node root, int columns, int rows, std::set<int> usedStates, long long targetHash, int* searchCost, long long p, long long x){
     std::queue<Node> nodeQueue;
 
     nodeQueue.push(root);
     while (!nodeQueue.empty()){
         Node nextNode = nodeQueue.front();
         // Push his state
-        usedStates.insert(nextNode.state);
+        usedStates.insert(nextNode.hash);
         nodeQueue.pop();
+        // std::cout<< "Root: "<< nextNode.state<< ", hash: "<< nextNode.hash<< endLine;
         for (Node expandedNode: nextNode.expandNode(columns, rows)){
-            if (expandedNode.state == targetState){
+            expandedNode.hash = expandedNode.computeHash(p, x, rows, columns);
+            // std::cout<< "State: "<< expandedNode.state<< ", hash: "<< expandedNode.hash<< endLine;
+            if (expandedNode.hash == targetHash){
                 return expandedNode;
             }
-            if (usedStates.find(expandedNode.state) == usedStates.end()){
+            if (usedStates.find(expandedNode.hash) == usedStates.end()){
                 (*searchCost) ++;
                 nodeQueue.push(expandedNode);
-                usedStates.insert(expandedNode.state);
+                usedStates.insert(expandedNode.hash);
             }
         }
     }
     // Return a node with the searchCost, but initial state and emptyPath
     std::vector<std::string> emptyPath;
-    std::vector<int> indices(rows*columns, 0);
+    std::array<int, 9> indices;
     Node notFound = Node(root.state, *searchCost, emptyPath, indices);
 
     return notFound;
--- bfs.hpp
+++ bfs.hpp
@@ -4,4 +4,4 @@
 #include <queue>
 #include <set>
 
-Node bfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost);
+Node bfs(Node root, int columns, int rows, std::set<int> usedStates, long long targetHash, int* searchCost, long long p, long long x);
