887d58ec1f572ca881372a199359fa4eeabb94a1
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-13 08:30:37 -0400

    Update: Check state to avoid repetitions and storing of the solution in a global variable

--- dfs.cpp
+++ dfs.cpp
@@ -1,41 +1,75 @@
 #include "dfs.hpp"
 
-Node dfs(Node root, int columns, int rows, std::set<int> usedStates, long long targetHash, int* searchCost, long long p, long long x){
+Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
     std::stack<Node> nodeStack;
     nodeStack.push(root);
+
+    int depth = -1;
+    int lastDepth;
+    int indexBlankPiece;
+    std::string newState;
+    std::list<std::string> solutionPath;
 
     while (!nodeStack.empty()){
         // Get last Node
         Node nextNode = nodeStack.top();
-        // Push his state
-        usedStates.insert(nextNode.hash);
-        nodeStack.pop();
-        for (Node expandedNode: nextNode.expandNode(columns, rows)){
-            expandedNode.hash = expandedNode.computeHash(p, x, rows, columns);
-            if (expandedNode.hash == targetHash){
-                return expandedNode;
-            }
-            if (usedStates.find(expandedNode.hash) == usedStates.end()){
-                (*searchCost)++;
-                nodeStack.push(expandedNode);
-                usedStates.insert(expandedNode.hash);
-            }
-            // All possible states were seen (https://stackoverflow.com/questions/32983617/improve-8-puzzle-using-bfs)
-            if (usedStates.size() >= 181440){
-                // Return a node with the searchCost, but initial state and emptyPath
-                std::vector<std::string> emptyPath;
-                std::array<int, 9> indices;
-                Node notFound = Node(root.state, *searchCost, emptyPath, indices);
-                return notFound;
-            }
+
+        lastDepth = depth;
+        depth = nextNode.cost;
+
+        // Check if came for a parent
+        if (depth < lastDepth){
+            solutionPath.pop_back();
         }
-        // std::cout << nodeStack.size()<< endLine;
+
+        nodeStack.pop();
+        indexBlankPiece = nextNode.index;
+
+        // Ignore moves for this state
+        if (usedStates.find(nextNode.state) != usedStates.end()){
+            continue;
+        }
+
+        // 
+        (*searchCost)++;
+        usedStates.insert(nextNode.state);
+        solutionPath.push_back(nextNode.state);
+
+        if (nextNode.state == targetState){
+            nextNode.path = solutionPath;
+            return nextNode;
+        }
+
+        // Move leftside
+        if (nextNode.index % columns != 0){
+            newState = nextNode.changeState(indexBlankPiece, indexBlankPiece-1);
+            Node node = Node(newState, nextNode.cost+1, nextNode.path, indexBlankPiece-1);
+            nodeStack.push(node);
+        }
+
+        // Move downside
+        if (indexBlankPiece < (rows-1)*columns){
+            newState = nextNode.changeState(indexBlankPiece, indexBlankPiece+columns);
+            Node node = Node(newState, nextNode.cost+1, nextNode.path, indexBlankPiece+columns);
+            nodeStack.push(node);
+        }
+
+        // Move rightside
+        if (indexBlankPiece % columns != columns-1){
+            newState = nextNode.changeState(indexBlankPiece, indexBlankPiece+1);
+            Node node = Node(newState, nextNode.cost+1, nextNode.path, indexBlankPiece+1);
+            nodeStack.push(node);
+        }
+
+        // Move upside
+        if (indexBlankPiece >= columns){
+            newState = nextNode.changeState(indexBlankPiece, indexBlankPiece-columns);
+            Node node = Node(newState, nextNode.cost, nextNode.path, indexBlankPiece-columns);
+            nodeStack.push(node);
+        }
     }
 
-    // Return a node with the searchCost, but initial state and emptyPath
-    std::vector<std::string> emptyPath;
-    std::array<int, 9> indices;
-    Node notFound = Node(root.state, *searchCost, emptyPath, indices);
+    Node notFound = Node(root.state, *searchCost, solutionPath, -1);
 
     return notFound;
 }
--- dfs.hpp
+++ dfs.hpp
@@ -4,4 +4,4 @@
 #include <set>
 #include <stack>
 
-Node dfs(Node root, int columns, int rows, std::set<int> usedStates, long long targetHash, int* searchCost, long long p, long long x);
+Node dfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost);
