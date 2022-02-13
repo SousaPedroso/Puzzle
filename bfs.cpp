b5b85af92eabf1e1719edba5ad95621488c17a22
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-02-13 08:32:29 -0400

    Update: Avoid repetitions through state

--- bfs.cpp
+++ bfs.cpp
@@ -1,32 +1,28 @@
 #include "bfs.hpp"
 
-Node bfs(Node root, int columns, int rows, std::set<int> usedStates, long long targetHash, int* searchCost, long long p, long long x){
+Node bfs(Node root, int columns, int rows, std::set<std::string> usedStates, std::string targetState, int* searchCost){
     std::queue<Node> nodeQueue;
 
     nodeQueue.push(root);
     while (!nodeQueue.empty()){
         Node nextNode = nodeQueue.front();
         // Push his state
-        usedStates.insert(nextNode.hash);
+        usedStates.insert(nextNode.state);
         nodeQueue.pop();
-        // std::cout<< "Root: "<< nextNode.state<< ", hash: "<< nextNode.hash<< endLine;
         for (Node expandedNode: nextNode.expandNode(columns, rows)){
-            expandedNode.hash = expandedNode.computeHash(p, x, rows, columns);
-            // std::cout<< "State: "<< expandedNode.state<< ", hash: "<< expandedNode.hash<< endLine;
-            if (expandedNode.hash == targetHash){
+            if (expandedNode.state == targetState){
                 return expandedNode;
             }
-            if (usedStates.find(expandedNode.hash) == usedStates.end()){
+            if (usedStates.find(expandedNode.state) == usedStates.end()){
                 (*searchCost) ++;
                 nodeQueue.push(expandedNode);
-                usedStates.insert(expandedNode.hash);
+                usedStates.insert(expandedNode.state);
             }
         }
     }
     // Return a node with the searchCost, but initial state and emptyPath
-    std::vector<std::string> emptyPath;
-    std::array<int, 9> indices;
-    Node notFound = Node(root.state, *searchCost, emptyPath, indices);
+    std::list<std::string> emptyPath;
+    Node notFound = Node(root.state, *searchCost, emptyPath, -1);
 
     return notFound;
 }
